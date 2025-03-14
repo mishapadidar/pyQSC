4c0c755eab5f64b38b44bd56eb2f563522268cbf
mishapadidar <mishapadidar14@gmail.com>
2025-03-14 17:09:50 -0400

    corrected aliasing error in mercier

--- qsc/mercier.py
+++ qsc/mercier.py
@@ -16,15 +16,15 @@ def mercier(self):
     # See also "20200604-02 Checking sign in Mercier DGeod near axis.docx"
 
     # Shorthand:
-    d_l_d_phi = self.d_l_d_phi
+    d_l_d_phi = torch.clone(self.d_l_d_phi)
     B0 = self.B0
-    G0 = self.G0
+    G0 = torch.clone(self.G0)
     p2 = self.p2
-    etabar = self.etabar
-    curvature = self.curvature
-    sigma = self.sigma
-    iotaN = self.iotaN
-    iota = self.iota
+    etabar = torch.clone(self.etabar)
+    curvature = torch.clone(self.curvature)
+    sigma = torch.clone(self.sigma)
+    iotaN = torch.clone(self.iotaN)
+    iota = torch.clone(self.iota)
     pi = torch.pi
 
     #integrand = d_l_d_phi * (Y1c * Y1c + X1c * (X1c + Y1s)) / (Y1c * Y1c + (X1c + Y1s) * (X1c + Y1s))
@@ -34,13 +34,17 @@ def mercier(self):
     integral = torch.sum(integrand) * self.d_phi * self.nfp * 2 * pi / self.axis_length
 
     #DGeod_times_r2 = -(2 * sG * spsi * mu0 * mu0 * p2 * p2 * G0 * G0 * G0 * G0 * etabar * etabar &
-    self.DGeod_times_r2 = -(2 * mu0 * mu0 * p2 * p2 * G0 * G0 * G0 * G0 * etabar * etabar \
+    DGeod_times_r2 = -(2 * mu0 * mu0 * p2 * p2 * G0 * G0 * G0 * G0 * etabar * etabar \
                        / (pi * pi * pi * B0 * B0 * B0 * B0 * B0 * B0 * B0 * B0 * B0 * B0 * iotaN * iotaN)) \
                        * integral
 
-    self.d2_volume_d_psi2 = 4*pi*pi*abs(G0)/(B0*B0*B0)*(3*etabar*etabar - 4*self.B20_mean/B0 + 2 * (self.G2 + iota * self.I2)/G0)
+    d2_volume_d_psi2 = 4*pi*pi*torch.abs(G0)/(B0*B0*B0)*(3*etabar*etabar - 4*torch.clone(self.B20_mean)/B0 + 2 * (torch.clone(self.G2) + iota * self.I2)/G0)
 
-    self.DWell_times_r2 = (mu0 * p2 * abs(G0) / (8 * pi * pi * pi * pi * B0 * B0 * B0)) * \
-        (self.d2_volume_d_psi2 - 8 * pi * pi * mu0 * p2 * abs(G0) / (B0 * B0 * B0 * B0 * B0))
+    DWell_times_r2 = (mu0 * p2 * torch.abs(G0) / (8 * pi * pi * pi * pi * B0 * B0 * B0)) * \
+        (d2_volume_d_psi2 - 8 * pi * pi * mu0 * p2 * torch.abs(G0) / (B0 * B0 * B0 * B0 * B0))
 
-    self.DMerc_times_r2 = self.DWell_times_r2 + self.DGeod_times_r2
+    self.DMerc_times_r2 = DWell_times_r2 + DGeod_times_r2
+    self.DWell_times_r2 = DWell_times_r2
+    self.d2_volume_d_psi2 = d2_volume_d_psi2
+    self.DGeod_times_r2 = DGeod_times_r2
+
