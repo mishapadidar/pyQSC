52f6f5484d1a3271cc42297f9b9f8637e472b112
mishapadidar <mishapadidar14@gmail.com>
2025-04-28 15:11:37 -0400

    updating stage 2 example to use coil-axis distance penalty and coil initialization around axis

--- examples/stage_2.py
+++ examples/stage_2.py
@@ -7,7 +7,8 @@ from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 from simsopt.geo import plot as sms_plot
 from qsc.simsopt_objectives import (QscOptimizable, ExternalFieldError,
-                                    GradExternalFieldError)
+                                    GradExternalFieldError, CurveAxisDistancePenalty)
+from qsc.simsopt_tools import create_equally_spaced_curves_around_axis
 
 """
 Stage 2 optimization example.
@@ -15,24 +16,22 @@ Stage 2 optimization example.
 
 # configuration parameters
 ncoils = 4
-nfp = 2
 is_stellsym = True
-coil_major_radius = 1.0
-coil_minor_radius = 0.5
-coil_n_fourier_modes = 3
+coil_n_fourier_modes = 8
 coil_current = 100000.0
 
 # last closed flux surface
 aspect_ratio = 10.0
 
 # coil constraint parameters
-coil_length_target = 3.0 # length of each coil
+coil_length_target = 3.5 # length of each coil
 coil_curvature_target = 5.0 
 cc_dist_target = 0.2 # coil separation
+coil_plasma_dist_target = 0.2 # coil-axis separation
 
 # optimization parameters
-max_iter = 100
-mu_penalty = 1.0
+max_iter = 500
+mu_penalty = 10.0
 
 """ initialization """
 
@@ -42,19 +41,18 @@ nfp = stel.nfp
 minor_radius = stel.rc[0].detach().numpy().item() / aspect_ratio
 
 # initialize coils
-base_curves = create_equally_spaced_curves(ncoils, nfp, stellsym=is_stellsym, R0=coil_major_radius,
-                                        R1=coil_minor_radius, order=coil_n_fourier_modes)
+coil_minor_radius = 4.0 * minor_radius
+base_curves = create_equally_spaced_curves_around_axis(stel, ncoils, is_stellsym, R1=coil_minor_radius, order=coil_n_fourier_modes)
 base_currents = [Current(1.0) * coil_current for i in range(ncoils)]
 coils = coils_via_symmetries(base_curves, base_currents, nfp, stellsym=is_stellsym)
 biot_savart = BiotSavart(coils)
-
 
 # plot the coils and axis
 xyz0 = stel.XYZ0.detach().numpy() # (3, nphi)
 ax = plt.figure().add_subplot(projection='3d')
 ax.plot(xyz0[0], xyz0[1], xyz0[2])
 sms_plot(coils, engine="matplotlib", ax=ax, close=True, show=False)
-surface = stel.surface(r=minor_radius, ntheta=64).detach().numpy()
+surface = stel.surface(r=minor_radius, ntheta=64).detach().numpy() # (nphi, ntheta, 3)
 ax.plot_surface(surface[:,:,0], surface[:,:,1], surface[:,:,2], alpha=0.3, color='lightgray')
 ax.set_xlim(-1.2, 1.2)
 ax.set_ylim(-1.2, 1.2)
@@ -76,10 +74,15 @@ fe = ExternalFieldError(biot_savart, stel, r=minor_radius, ntheta=ntheta_vc, nph
 ge = GradExternalFieldError(biot_savart, stel, r=minor_radius, ntheta=ntheta_vc, nphi=nphi_vc, ntarget=ntarget)
 
 # coil constraints/regularization
-coil_lengths_penalties = [(1 / coil_length_target**2) * QuadraticPenalty(CurveLength(c), coil_length_target, "identity") for c in base_curves]
+coil_lengths_penalties = [(1 / coil_length_target**2) * QuadraticPenalty(CurveLength(c), coil_length_target, "max") for c in base_curves]
 coil_curvature_penalties = [(1 / coil_curvature_target**2) * LpCurveCurvature(c, 2, threshold=coil_curvature_target) for c in base_curves]
 msc_penalties = [(1 / coil_curvature_target**2) * QuadraticPenalty(MeanSquaredCurvature(c), coil_curvature_target, "max") for c in base_curves]
 cc_dist_penalty = CurveCurveDistance([c.curve for c in coils], cc_dist_target)
+
+# coil-axis distance penalty
+axis_plasma_dist = np.max(np.linalg.norm(surface - xyz0.T[:, None, :], axis=-1))
+coil_axis_dist_target = axis_plasma_dist + coil_plasma_dist_target
+coil_axis_dist_penalty = [(1 / coil_axis_dist_target**2) * CurveAxisDistancePenalty(c, stel, coil_axis_dist_target) for c in base_curves]
 
 # form an Optimizable objective
 prob = (fe 
@@ -88,6 +91,7 @@ prob = (fe
         + mu_penalty * sum(coil_curvature_penalties)
         + mu_penalty * sum(msc_penalties)
         + mu_penalty * cc_dist_penalty
+        + mu_penalty * sum(coil_axis_dist_penalty)
         )
 def fun(dofs):
     biot_savart.x = dofs
