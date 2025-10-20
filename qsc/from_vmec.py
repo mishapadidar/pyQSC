c7c207d2622b254764b0b3905aa0532b12c97dc2
mishapadidar <mishapadidar14@gmail.com>
2025-10-20 16:33:37 -0400

    got the from_vmec function working for the QH. Added unit tests

--- qsc/from_vmec.py
+++ qsc/from_vmec.py
@@ -5,11 +5,17 @@ from a VMEC equilibrium.
 """
 
 @classmethod
-def from_vmec(cls, v, n_fourier=5, **kwargs):
-    """Build a Qsc object from a VMEC equilibria. Since Qsc represents quasisymmetric
-    equilbria, the VMEC equilibria should be close to quasisymmetric on the magnetic axis.
+def from_vmec(cls, v, n_fourier=10, **kwargs):
+    """Build a Qsc object from a VMEC equilibria. To ensure a good fit, the
+    VMEC equilibria should be precisely quasisymmetric on the magnetic axis.
     Furthermore, if pressure and current profiles are used that have large O(s^2) terms,
     the resulting Qsc object may not accurately represent the equilibrium.
+
+    NOTE: The Boozer poloidal angle used by BoozXform differs from the
+    near-axis poloidal angle by a shift and sign reversal. This is accounted for
+    in the calculation of etabar, B2c, and B2s below. This means that iota_vmec
+    and iota_nae differ by a sign, and furthermore that the contours of |B|
+    will be mirrored in the poloidal angle.
 
     Currently only stellarator-symmetric equilibria are supported.
 
@@ -20,15 +26,19 @@ def from_vmec(cls, v, n_fourier=5, **kwargs):
     ----
         v (Vmec): An instance of the Simsopt Vmec class.
         n_fourier (int): Highest Fourier mode number to use for the axis shape.
+            Default is 10.
         **kwargs: Additional keyword arguments passed to the Qsc constructor.
 
     Returns
     -------
         self (Qsc): The Qsc object.
     """
-    from simsopt.mhd.vmec_diagnostics import vmec_compute_geometry
-    from simsopt.geo import CurveRZFourier
-    from simsopt.mhd import Boozer
+    try:
+        from simsopt.mhd.vmec_diagnostics import vmec_compute_geometry
+        from simsopt.geo import CurveRZFourier
+        from simsopt.mhd import Boozer
+    except ImportError:
+        raise ImportError("The 'simsopt' library is required to use this from_vmec.")
 
     # ensure VMEC has run
     v.run()
@@ -50,59 +60,31 @@ def from_vmec(cls, v, n_fourier=5, **kwargs):
     if not stellsym:
         raise NotImplementedError("from_vmec currently only supports stellarator-symmetric configurations.")
 
-    print('nfp', nfp)
-    print('stellsym', stellsym)
-
-    s = np.linspace(1e-8, 1, 32, endpoint=True)
+    s = np.linspace(1e-8, 1, 64, endpoint=True)
     ntheta_vmec = 63
     nphi_vmec = 2 * n_fourier + 1
-    theta = np.linspace(0, 2 * np.pi, ntheta_vmec)
-    phi = np.linspace(0, 2 * np.pi / nfp, nphi_vmec)
-    data = vmec_compute_geometry(v, s, theta, phi)
+    theta_vmec = np.linspace(0, 2 * np.pi, ntheta_vmec)
+    phi_vmec = np.linspace(0, 2 * np.pi / nfp, nphi_vmec)
+    data = vmec_compute_geometry(v, s, theta_vmec, phi_vmec)
 
     # extract the axis shape
     X = data.X[0].reshape((ntheta_vmec, nphi_vmec)).mean(axis=0) # (nphi,)
     Y = data.Y[0].reshape((ntheta_vmec, nphi_vmec)).mean(axis=0) # (nphi,)
     Z = data.Z[0].reshape((ntheta_vmec, nphi_vmec)).mean(axis=0) # (nphi,)
 
-    # VMEC uses a different sign convention for Z
-    Z = -Z
+    # TODO: make sure the axis is increasing in phi. Otherwise
+    # TODO: we need to reorder the points.
 
     xyz = np.array([X, Y, Z]).T # (nphi, 3)
-    curve = CurveRZFourier(quadpoints=phi/2/np.pi, order = n_fourier, nfp=nfp, stellsym=stellsym)
+    curve = CurveRZFourier(quadpoints=phi_vmec/2/np.pi, order = n_fourier, nfp=nfp, stellsym=stellsym)
     curve.unfix_all()
     curve.least_squares_fit(xyz)
     rc = curve.x[:n_fourier+1]
     zs = np.append([0.0], curve.x[n_fourier+1:])
 
-    print('rc')
-    print(rc)
-    print('zs')
-    print(zs)
-
-    ## sanity check: plot the axis shape
-    # from simsopt.geo import plot
-    # from mpl_toolkits.mplot3d import Axes3D
-    # import matplotlib.pyplot as plt
-    # curve1 = CurveRZFourier(quadpoints=99, order = n_fourier, nfp=nfp, stellsym=stellsym)
-    # curve1.x = curve.x
-    # add_default_args(kwargs, rc=rc, zs=zs, nfp=nfp, etabar=0.1, B0=1,
-    #              nphi=99, order="r1")
-    # stel =  cls(**kwargs)
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111, projection='3d')
-    # xyz = stel.XYZ0.detach().numpy()
-    # ax.plot(xyz[0], xyz[1], xyz[2], label='Axis Shape')
-    # ax.set_xlabel('X')
-    # ax.set_ylabel('Y')
-    # ax.set_zlabel('Z')
-    # plot([curve1], ax = ax, show=True)
-
     # Get B0
-    print('modB shape', data.modB.shape)
     modB = data.modB[0] # (ntheta, nphi)
     B0 = modB.mean()
-    print("B0", B0)
 
     # get the profiles
     """
@@ -122,98 +104,130 @@ def from_vmec(cls, v, n_fourier=5, **kwargs):
     The near axis current profile is 
         I(r) = I2 * r^2.
     We get the I2 from curtor,
-        curtor = 2 * np.pi / (mu0 * I2 * a**2).
+        curtor = (2 * np.pi / mu0) * I2 * a**2.
     """
-    phiedge = v.wout.phi[-1]
-    # # TODO: check sign of toroidal flux vs sign of phiedge
-    # print("toroidal_flux_sign", data.toroidal_flux_sign)
-    # print("sign(phiedge)", np.sign(phiedge))
 
     # get the minor radius through phiedge = spsi * pi * a^2 * B0
-    # spsi = data.toroidal_flux_sign
+    phiedge = v.wout.phi[-1]
     spsi = np.sign(phiedge)
     aminor = np.sqrt(np.abs(phiedge) / (np.pi * B0))
 
     dp_ds = data.d_pressure_d_s # (ns,)
     p2 = (dp_ds[0] * spsi * np.pi * B0) / phiedge
-    print('dp_ds', dp_ds)
 
     # get I2
-    # TODO: double check ctor is curtor
-    print('curtor', v.wout.ctor)
     mu0 = 4 * np.pi * 1e-7
-    I2 = (v.wout.ctor * mu0 * aminor**2) / (2 * np.pi)
+    I2 = (v.wout.ctor * mu0) / (2 * np.pi * aminor**2)
     if np.isclose(I2, 0.0, atol=1e-15):
         I2 = 0.0
-    print('I2', I2)
+
+    """
+    Now we compute the field strength in boozer coordinates using the BoozXform class.
+    We will use this to determine etabar, B2c, and B2s.
+    """
 
     booz = Boozer(v)
-    def modB_boozer(rs, ntheta=65, nphi=65):
+    def modB_boozer(r, ntheta=65, nphi=65):
+        """Compute |B| in Boozer coordinates.
+
+        The BoozXform theta differs from the near-axis theta by a shift and
+        reversal, 
+            theta_booz = - theta_nae + pi.
+        We determined this empirically from the cross sections and |B| from the 
+        BoozXform and the NAE.
         
-        s = (rs / aminor) ** 2
-        booz.register(s) # register surface
+
+        Args:
+            rs (float): minor radius to compute |B| on.
+            ntheta (int, optional): number of theta quadpoints. Defaults to 65.
+            nphi (int, optional): number of phi quadpoints. Defaults to 65.
+
+        Returns:
+            (B, R, Z): (nphi, ntheta) arrays of |B|, R, and Z on the specified surface.
+        """
+        
+        s = (r / aminor) ** 2
+        booz.register([s]) # register surface
         booz.run()
         bx = booz.bx
 
         # discretize boozer angles
-        theta1d = np.linspace(0, 2 * np.pi, ntheta, endpoint=False)
+        theta1d = np.linspace(3*np.pi, np.pi, ntheta, endpoint=False)
+        # theta1d = np.linspace(0, 2 * np.pi, ntheta, endpoint=False)
         phi1d = np.linspace(0, 2 * np.pi / nfp, nphi, endpoint=False)
         phi, theta = np.meshgrid(phi1d, theta1d, indexing='ij')
 
         # reconstruct |B|
-        modB = np.zeros((len(s), *np.shape(phi)))
-        for js in range(len(s)):
-            for jmn in range(len(bx.xm_b)):
-                m = bx.xm_b[jmn]
-                n = bx.xn_b[jmn]
-                angle = m * theta - n * phi
-                modB[js] += bx.bmnc_b[jmn, js] * np.cos(angle)
-                if bx.asym:
-                    modB[js] += bx.bmns_b[jmn, js] * np.sin(angle)
+        modB = np.zeros(np.shape(phi))
+        R = np.zeros(np.shape(phi))
+        Z = np.zeros(np.shape(phi))
+        js = 0
+        for jmn in range(len(bx.xm_b)):
+            m = bx.xm_b[jmn]
+            n = bx.xn_b[jmn]
+            angle = m * theta - n * phi
+            modB += bx.bmnc_b[jmn, js] * np.cos(angle)
+            R += bx.rmnc_b[jmn, js] * np.cos(angle)
+            Z += bx.zmns_b[jmn, js] * np.sin(angle)
+            if bx.asym:
+                modB += bx.bmns_b[jmn, js] * np.sin(angle)
+                R += bx.rmns_b[jmn, js] * np.sin(angle)
+                Z += bx.zmnc_b[jmn, js] * np.cos(angle)
 
-        return modB # (nphi, ntheta)
+        return modB, R, Z # (nphi, ntheta)
     
-    # TODO: what surface should we do this on?
+    # analysis is on LCFS
     r = aminor
     ntheta_booz = 65
-    nphi_booz = 65
-    modB = modB_boozer(np.array([r]), ntheta=ntheta_booz, nphi=nphi_booz)[0] # (ntheta, nphi)
+    nphi_booz = 63
+    modB, _, _ = modB_boozer(r, ntheta=ntheta_booz, nphi=nphi_booz) # (nphi, ntheta)
+
+    # determine sG
+    sG = np.sign(booz.bx.Boozer_G[0])
+
+    # determine helicity and N
+    temp = cls(rc=rc, zs=zs, nfp=nfp, B0=B0, order="r1", spsi=spsi, sG=sG)
+    helicity = round(-temp.helicity.detach().numpy())
+    N = helicity * nfp
 
     """
     etabar is a free parameter for the first order expansion.
-    We get it from the BoozXform since
-        B = B0 * (1 + etabar * r * cos(theta) + O(r^2))
+    We determine it from the BoozXform since
+        B = B0 * (1 + etabar * r * cos(vartheta) + O(r^2))
+    where vartheta = theta - N * phi is the helical angle.
+    We compute etabar by a least-squares fit.
     """
-    # get etabar
-    B_theta_fft = np.fft.fft((modB / B0 - 1) / r, axis=-1)
-    etabar = (2 / ntheta_booz) * np.real(B_theta_fft[:, 1]).mean()
+    # theta1d is the NAE theta
+    theta1d = np.linspace(0, 2 * np.pi, ntheta_booz, endpoint=False)
+    phi1d = np.linspace(0, 2 * np.pi / nfp, nphi_booz, endpoint=False)
+    phi, theta = np.meshgrid(phi1d, theta1d, indexing='ij')
+    vartheta = theta - N * phi
+    x = B0 * r * np.cos(vartheta)
+    etabar = np.mean((modB - B0) * x) / np.mean(x**2)
 
     """
     Next we will get B2c and B2s which are constants of the field strength
     B(r, theta, phi) expansion to second order in r:
-        B(r, theta, phi) = B0 * (1 + etabar * r * cos(theta) + r^2 * (B2c * cos(2*theta) + B2s * sin(2*theta)) + O(r^3)
+        B(r, theta, phi) = B0 * (1 + etabar * r * cos(vartheta) 
+            + r^2 * (B20(varphi) + B2c * cos(2 * vartheta) + B2s * sin(2 * vartheta)) + O(r^3)
     
     For stellarator-symmetric equilibria, B2s = 0.
+    We compute B2c and B2s by a least-squares fit.
     """
-    # compute B2c and B2s
-    B1 = B0 * (1 + etabar * r * np.cos(np.linspace(0, 2 * np.pi, ntheta_booz, endpoint=False)))
-    B_theta_fft = np.fft.fft((modB - B1) / r / r, axis=-1)
-    B2c = (2 / ntheta_booz) * np.real(B_theta_fft[:, 2]).mean() 
+    modB_shifted = modB - (B0 * (1 + r * etabar * np.cos(vartheta)) )
+    B20_times_r_squared = np.mean(modB_shifted, axis=1, keepdims=True) # (nphi, 1)
+    modB_shifted = modB_shifted - B20_times_r_squared
+    x = r**2 * np.cos(2 * (vartheta))
+    B2c = np.mean((modB_shifted) * x) / np.mean(x**2)
     if stellsym:
         B2s = 0.0
     else:
-        B2s = (-2 / ntheta_booz) * np.imag(B_theta_fft[:, 2]).mean()
-    print('B2c', B2c)
-    print('B2s', B2s)
-
-    # TODO: do we need to set sG?
+        x = r * r * np.sin(2 * (vartheta))
+        B2s = np.mean((modB_shifted) * x) / np.mean(x**2)
 
     add_default_args(kwargs, rc=rc, zs=zs, nfp=nfp, etabar=etabar, B0=B0,
-                 I2=I2, nphi=99, B2s=B2s, B2c=B2c, p2=p2, order="r3",
-                 spsi = spsi)
+                 I2=I2, nphi=nphi_booz, B2s=B2s, B2c=B2c, p2=p2, order="r3",
+                 spsi=spsi, sG=sG)
     stel = cls(**kwargs)
-
-    print('vmec iota', data.iota[0])
-    print('stel iota', stel.iota)
 
     return stel
--- qsc/tests/test_from_vmec.py
+++ qsc/tests/test_from_vmec.py
@@ -1,3 +1,5 @@
+import torch
+import numpy as np
 
 def generate_data_for_test_from_vmec():
     """
@@ -11,12 +13,204 @@ def test_from_vmec():
     """
     Test the from_vmec method of the Qsc class.
     """
-    # load a vmec equilibria
+
     from simsopt.mhd.vmec import Vmec
     from qsc.qsc import Qsc
-    v = Vmec('./data/wout_LandremanPaul2021_QA.nc')
 
-    Qsc.from_vmec(v, n_fourier=9)
+    """
+    First test that we can reproduce the precise QA configuration.
+    We compare against the Qsc.from_paper('precise QA') method.
+    This configuration is vacuum.
+    """
+
+    stel_actual = Qsc.from_paper("precise QA")
+    filename = "./input.precise_QA"
+    params = {
+        "ns_array": [12, 25, 50, 75, 100, 150, 201],
+        "niter_array": [1200, 2000, 3000, 4000, 5000, 6000, 20000],
+        "ftol_array": [1.0e-17, 1.0e-17, 1.0e-17, 1.0e-17, 1.0e-17, 1.0e-17, 5.0e-16]
+    }
+    stel_actual.to_vmec(filename, params=params)
+    # filename = "./data/wout_precise_QA.nc"
+    v = Vmec(filename)
+    stel = Qsc.from_vmec(v, n_fourier=len(stel_actual.rc)-1)
+
+    rc_actual = stel_actual.rc.detach().numpy()
+    rc = stel.rc.detach().numpy()
+    err = np.max(np.abs(rc - rc_actual)) 
+    np.testing.assert_almost_equal(rc, rc_actual,
+                                   err_msg="rc difference too large", decimal=3)
+    
+    zs_actual = stel_actual.zs.detach().numpy()
+    zs = stel.zs.detach().numpy()
+    err = np.max(np.abs(zs - zs_actual))
+    np.testing.assert_almost_equal(zs, zs_actual,
+                                   err_msg="zs difference too large", decimal=3)
+    
+    etabar_actual = stel_actual.etabar.detach().numpy()
+    etabar = stel.etabar.detach().numpy()
+    err = np.max(np.abs(etabar - etabar_actual))
+    np.testing.assert_almost_equal(etabar, etabar_actual,
+                                   err_msg="etabar difference too large", decimal=1)
+    
+    B2c_actual = stel_actual.B2c.detach().numpy()
+    B2c = stel.B2c.detach().numpy()
+    err = np.max(np.abs(B2c - B2c_actual))
+    np.testing.assert_almost_equal(B2c, B2c_actual,
+                                   err_msg="B2c difference too large", decimal=1)
+    
+    B2s_actual = stel_actual.B2s.detach().numpy()
+    B2s = stel.B2s.detach().numpy()
+    err = np.max(np.abs(B2s - B2s_actual))
+    np.testing.assert_almost_equal(B2s, B2s_actual,
+                                   err_msg="B2s difference too large", decimal=14)
+    
+    p2_actual = stel_actual.p2.detach().numpy()
+    p2 = stel.p2.detach().numpy()
+    err = np.max(np.abs(p2 - p2_actual))
+    np.testing.assert_almost_equal(p2, p2_actual,
+                                   err_msg="p2 difference too large", decimal=1)
+    
+    I2_actual = stel_actual.I2.detach().numpy()
+    I2 = stel.I2.detach().numpy()
+    err = np.max(np.abs(I2 - I2_actual))
+    np.testing.assert_almost_equal(I2, I2_actual,
+                                   err_msg="I2 difference too large", decimal=3)
+        
+    B0_actual = stel_actual.B0
+    B0 = stel.B0
+    B0_error = abs(B0 - B0_actual)
+    np.testing.assert_almost_equal(B0, B0_actual,
+                                  err_msg="B0 difference too large", decimal=3)
+    
+    spsi_actual = stel_actual.spsi
+    spsi = stel.spsi
+    spsi_error = abs(spsi - spsi_actual)
+    np.testing.assert_almost_equal(spsi, spsi_actual,
+                                  err_msg="spsi difference too large", decimal=14)
+    
+    nfp_actual = stel_actual.nfp
+    nfp = stel.nfp
+    nfp_error = abs(nfp - nfp_actual)
+    np.testing.assert_almost_equal(nfp, nfp_actual,
+                                  err_msg="nfp difference too large", decimal=14)
+    
+    sG_actual = stel_actual.sG
+    sG = stel.sG
+    sG_error = abs(sG - sG_actual)
+    np.testing.assert_almost_equal(sG, sG_actual,
+                                  err_msg="sG difference too large", decimal=14)
+
+    # Print max absolute error for sigma0
+    iota_actual = stel_actual.iota.detach().numpy()
+    iota = stel.iota.detach().numpy()
+    iota_error = abs(iota - iota_actual)
+    np.testing.assert_almost_equal(iota, iota_actual,
+                                  err_msg="iota difference too large", decimal=1)
+    
+    # Print max absolute error for sigma0
+    helicity_actual = stel_actual.helicity.detach().numpy()
+    helicity = stel.helicity.detach().numpy()
+    helicity_error = abs(helicity - helicity_actual)
+    np.testing.assert_almost_equal(helicity, helicity_actual,
+                                  err_msg="helicity difference too large", decimal=14)
+
+
+    """
+    Test a QH with pressure and current.
+    """
+    stel_actual = Qsc.from_paper("precise QH", p2 = -1e2, I2=-1e-6)
+    filename = "./input.precise_QH"
+    params = {
+        "ns_array": [12, 25, 50, 75, 100, 150, 201],
+        "niter_array": [1200, 2000, 3000, 4000, 5000, 6000, 20000],
+        "ftol_array": [1.0e-17, 1.0e-17, 1.0e-17, 1.0e-17, 1.0e-17, 1.0e-17, 5.0e-16]
+    }
+    stel_actual.to_vmec(filename, params=params)
+    # filename = "./data/wout_precise_QH.nc"
+    v = Vmec(filename)
+    stel = Qsc.from_vmec(v, n_fourier=len(stel_actual.rc)-1)
+
+    rc_actual = stel_actual.rc.detach().numpy()
+    rc = stel.rc.detach().numpy()
+    err = np.max(np.abs(rc - rc_actual)) 
+    np.testing.assert_almost_equal(rc, rc_actual,
+                                   err_msg="rc difference too large", decimal=3)
+    
+    zs_actual = stel_actual.zs.detach().numpy()
+    zs = stel.zs.detach().numpy()
+    err = np.max(np.abs(zs - zs_actual))
+    np.testing.assert_almost_equal(zs, zs_actual,
+                                   err_msg="zs difference too large", decimal=3)
+    
+    etabar_actual = stel_actual.etabar.detach().numpy()
+    etabar = stel.etabar.detach().numpy()
+    err = np.max(np.abs(etabar - etabar_actual))
+    np.testing.assert_almost_equal(etabar, etabar_actual,
+                                   err_msg="etabar difference too large", decimal=1)
+    
+    B2c_actual = stel_actual.B2c.detach().numpy()
+    B2c = stel.B2c.detach().numpy()
+    err = np.max(np.abs(B2c - B2c_actual))
+    np.testing.assert_almost_equal(B2c, B2c_actual,
+                                   err_msg="B2c difference too large", decimal=1)
+    
+    B2s_actual = stel_actual.B2s.detach().numpy()
+    B2s = stel.B2s.detach().numpy()
+    err = np.max(np.abs(B2s - B2s_actual))
+    np.testing.assert_almost_equal(B2s, B2s_actual,
+                                   err_msg="B2s difference too large", decimal=14)
+    
+    p2_actual = stel_actual.p2.detach().numpy()
+    p2 = stel.p2.detach().numpy()
+    err = np.max(np.abs(p2 - p2_actual))
+    np.testing.assert_almost_equal(p2, p2_actual,
+                                   err_msg="p2 difference too large", decimal=1)
+    
+    I2_actual = stel_actual.I2.detach().numpy()
+    I2 = stel.I2.detach().numpy()
+    err = np.max(np.abs(I2 - I2_actual))
+    np.testing.assert_almost_equal(I2, I2_actual,
+                                   err_msg="I2 difference too large", decimal=3)
+        
+    B0_actual = stel_actual.B0
+    B0 = stel.B0
+    B0_error = abs(B0 - B0_actual)
+    np.testing.assert_almost_equal(B0, B0_actual,
+                                  err_msg="B0 difference too large", decimal=3)
+    
+    spsi_actual = stel_actual.spsi
+    spsi = stel.spsi
+    spsi_error = abs(spsi - spsi_actual)
+    np.testing.assert_almost_equal(spsi, spsi_actual,
+                                  err_msg="spsi difference too large", decimal=14)
+    
+    nfp_actual = stel_actual.nfp
+    nfp = stel.nfp
+    nfp_error = abs(nfp - nfp_actual)
+    np.testing.assert_almost_equal(nfp, nfp_actual,
+                                  err_msg="nfp difference too large", decimal=14)
+    
+    sG_actual = stel_actual.sG
+    sG = stel.sG
+    sG_error = abs(sG - sG_actual)
+    np.testing.assert_almost_equal(sG, sG_actual,
+                                  err_msg="sG difference too large", decimal=14)
+
+    # Print max absolute error for sigma0
+    iota_actual = stel_actual.iota.detach().numpy()
+    iota = stel.iota.detach().numpy()
+    iota_error = abs(iota - iota_actual)
+    np.testing.assert_almost_equal(iota, iota_actual,
+                                  err_msg="iota difference too large", decimal=1)
+    
+    # Print max absolute error for sigma0
+    helicity_actual = stel_actual.helicity.detach().numpy()
+    helicity = stel.helicity.detach().numpy()
+    helicity_error = abs(helicity - helicity_actual)
+    np.testing.assert_almost_equal(helicity, helicity_actual,
+                                  err_msg="helicity difference too large", decimal=14)
+
 
 if __name__ == "__main__":
     # generate_data_for_test_from_vmec()
